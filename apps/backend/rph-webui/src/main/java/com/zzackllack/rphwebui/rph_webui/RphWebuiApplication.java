403cb27069c3e3cefd820a9e7d35e7f612bccead
Zacklack <contact@zacklack.de>
2025-07-11 14:29:17 +0200

    chore: add NextJS frontend and Java Spring Boot backend

---
+++ apps/backend/rph-webui/src/main/java/com/zzackllack/rphwebui/rph_webui/RphWebuiApplication.java
@@ -0,0 +1,13 @@
+package com.zzackllack.rphwebui.rph_webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RphWebuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RphWebuiApplication.class, args);
+	}
+
+}
---
+++ apps/backend/rph-webui/src/test/java/com/zzackllack/rphwebui/rph_webui/RphWebuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzackllack.rphwebui.rph_webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RphWebuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
